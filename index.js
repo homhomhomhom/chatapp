1072f6cc27103820c123b6fca84c340b5789565e
hom <unkel.dylan@mgmail.com>
2020-07-01 16:08:24 +0200

    nu dan?

--- index.js
+++ index.js
@@ -25,38 +25,40 @@ app.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/public/index.html');
 // });
 
-app.get('/javascript', (req, res) => {
-    res.sendFile(__dirname + '/public/javascript.html');
-});
+// app.get('/javascript', (req, res) => {
+//     res.sendFile(__dirname + '/public/javascript.html');
+// });
 
-app.get('/swift', (req, res) => {
-    res.sendFile(__dirname + '/public/swift.html');
-});
+// app.get('/swift', (req, res) => {
+//     res.sendFile(__dirname + '/public/swift.html');
+// });
 
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/public/css.html');
-});
+// app.get('/css', (req, res) => {
+//     res.sendFile(__dirname + '/public/css.html');
+// });
 
-// tech namespace
-const tech = io.of('/tech');
+// // tech namespace
+// const tech = io.of('/tech');
 
-tech.on('connection', (socket) => {
-    socket.on('join', (data) => {
-        socket.join(data.room);
-        tech.in(data.room).emit('message', `New user joined ${data.room} room!`);
-    })
+// tech.on('connection', (socket) => {
+//     socket.on('join', (data) => {
+//         socket.join(data.room);
+//         tech.in(data.room).emit('message', `New user joined ${data.room} room!`);
+//     })
 
-    socket.on('message', (data) => {
-        console.log(`message: ${data.msg}`);
-        tech.in(data.room).emit('message', data.msg);
-    });
+//     socket.on('message', (data) => {
+//         console.log(`message: ${data.msg}`);
+//         tech.in(data.room).emit('message', data.msg);
+//     });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
 
-        tech.emit('message', 'user disconnected');  
-    })
-})
+//         tech.emit('message', 'user disconnected');  
+//     })
+// })
+
+
 // const express = require('express');
 // const app = express();
 // var server = app.listen((process.env.PORT || 4001), function(){ console.log('APP Loaded!'); });

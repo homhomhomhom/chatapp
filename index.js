4c606e26d4cd8985ca1a81f176cde11bd1a9539f
hom <unkel.dylan@mgmail.com>
2020-07-01 13:54:45 +0200

    mogelijke fix 2?

--- index.js
+++ index.js
@@ -1,45 +1,61 @@
-const app = require('express')(server);   
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const port = process.env.PORT || 3000;
+// const app = require('express')(server);   
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
+// const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// server.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// });
+
+// app.get('/javascript', (req, res) => {
+//     res.sendFile(__dirname + '/public/javascript.html');
+// });
+
+// app.get('/swift', (req, res) => {
+//     res.sendFile(__dirname + '/public/swift.html');
+// });
+
+// app.get('/css', (req, res) => {
+//     res.sendFile(__dirname + '/public/css.html');
+// });
+
+// // tech namespace
+// const tech = io.of('/tech');
+
+// tech.on('connection', (socket) => {
+//     socket.on('join', (data) => {
+//         socket.join(data.room);
+//         tech.in(data.room).emit('message', `New user joined ${data.room} room!`);
+//     })
+
+//     socket.on('message', (data) => {
+//         console.log(`message: ${data.msg}`);
+//         tech.in(data.room).emit('message', data.msg);
+//     });
+
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
+
+//         tech.emit('message', 'user disconnected');  
+//     })
+// })
+const express = require('express');
+const app = express();
+var server = app.listen((process.env.PORT || 4001), function(){ console.log('APP Loaded!'); });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+
+//
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
 
-app.get('/javascript', (req, res) => {
-    res.sendFile(__dirname + '/public/javascript.html');
-});
+    res.send('ok hij doet t...');
 
-app.get('/swift', (req, res) => {
-    res.sendFile(__dirname + '/public/swift.html');
-});
-
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/public/css.html');
-});
-
-// tech namespace
-const tech = io.of('/tech');
-
-tech.on('connection', (socket) => {
-    socket.on('join', (data) => {
-        socket.join(data.room);
-        tech.in(data.room).emit('message', `New user joined ${data.room} room!`);
-    })
-
-    socket.on('message', (data) => {
-        console.log(`message: ${data.msg}`);
-        tech.in(data.room).emit('message', data.msg);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-
-        tech.emit('message', 'user disconnected');  
-    })
-})
+}

b9501c164e3f16a725d67ca42ae07265d5797552
hom <unkel.dylan@mgmail.com>
2020-07-07 15:14:31 +0200

    NU DAN?

--- queries.js
+++ queries.js
@@ -1,6 +1,6 @@
 const pg = require('pg');
 const process_db = require('dotenv').config();
-const db_url = process.env.DATABASE_URL || process_db.parsed.DB_URL;
+const db_url = process.env.DATABASE_URL || process_db.parsed.DATABASE_URL;
 const client = new pg.Client({
     connectionString: db_url,
     ssl: { rejectUnauthorized: false }
